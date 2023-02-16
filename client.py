0c2c0df956a0775df8364a2debe56f3886271706
Divya <divya.amirtharaj@gmail.com>
2023-02-15 19:28:26 -0500

    set client

--- client.py
+++ client.py
@@ -1,34 +1,8 @@
-import grpc
-import protos.service_pb2_grpc as pb2_grpc
-import protos.service_pb2 as pb2
+import threading
 
+class Client:
+    def __init__(self) -> None:
+        pass
 
-class UnaryClient(object):
-    """
-    Client for gRPC functionality
-    """
-
-    def __init__(self):
-        self.host = 'localhost'
-        self.server_port = 50051
-
-        # instantiate a channel
-        self.channel = grpc.insecure_channel(
-            '{}:{}'.format(self.host, self.server_port))
-
-        # bind the client and the server
-        self.stub = pb2_grpc.UnaryStub(self.channel)
-
-    def get_url(self, message):
-        """
-        Client function to call the rpc for GetServerResponse
-        """
-        message = pb2.Message(message=message)
-        print(f'{message}')
-        return self.stub.GetServerResponse(message)
-
-
-if __name__ == '__main__':
-    client = UnaryClient()
-    result = client.get_url(message="Hello Server you there?")
-    print(f'{result}')
+    def send_message(self):
+        pass
--- server.py
+++ server.py
@@ -7,14 +7,23 @@ import protos.service_pb2 as pb2
 
 class Server(pb2_grpc.ChatBotServicer):
     def __init__(self) -> None:
+        self.history = []
         pass
+
+    def start_stream(self, iterator, context):
+        last_message = 0
+        while True:
+            while len(self.history) > last_message:
+                n = self.history[last_message]
+                last_message += 1
+                yield n
     
     def send_chat(self, request: pb2.Chat, context):
         print(print("[{}] {}".format(request.name, request.message)))
         return pb2.Empty()
 
 if __name__ == '__main__':
-    port = 7172001
+    port = 11921
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))  # create a gRPC server
     pb2_grpc.add_ChatBotServicer_to_server(Server, server)  # register the server to gRPC
     print('Starting server. Listening...')

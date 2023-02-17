0d6779af103823b746645630a7b2619cd9331ab9
Divya <divya.amirtharaj@gmail.com>
2023-02-17 01:33:21 -0500

    hm

--- client.py
+++ client.py
@@ -9,20 +9,38 @@ address = "localhost"
 port = 11912
 
 class Client:
-    def __init__(self, user: str) -> None:
+
+    def __init__(self, user: str):
         self.username = user
-        channel = grpc.insecure_channel('localhost:11912', options=(('grpc.enable_http_proxy', 0),))
+        #channel = grpc.insecure_channel('localhost:11912', options=(('grpc.enable_http_proxy', 0),))
+        channel = grpc.insecure_channel('localhost:11912')
         self.stub = pb2_grpc.ChatBotStub(channel)
+        threading.Thread(target=self.__get_message, daemon=True).start()
 
     def send_message(self):
         # send messages to server
         msg = sys.stdin.readline()
-        n = pb2.Chat()
-        n.username = self.username
-        n.message = msg
-        print(type(n))
-        self.stub.send_chat(n)
+        if msg != "":
+            n = pb2.Chat()
+            n.username = self.username
+            n.message = msg
+            print("[{}] {}".format(n.username, n.message))
+            self.stub.send_chat(n)
 
-c = Client("divya")
-c.send_message()
-time.sleep(64 * 64 * 100)
+    def __get_message(self):
+        for c in self.stub.send_chat(pb2.Empty()):
+            print("S[{}] {}".format(c.name, c.message))
+
+
+if __name__ == '__main__':
+    username = None
+    while username is None:
+        print("What's your username? \n")
+        username = input()
+    c = Client(username) 
+    c.send_message()
+    time.sleep(100*64*64)
+
+# c = Client("divya")
+#c.send_message()
+# time.sleep(64 * 64 * 100)
--- server.py
+++ server.py
@@ -1,23 +1,36 @@
-import grpc
 from concurrent import futures
+
+import grpc
+import time
+
 import protos.service_pb2_grpc as pb2_grpc
 import protos.service_pb2 as pb2
 
 
 class Server(pb2_grpc.ChatBotServicer):
-    def __init__(self) -> None:
-        self.history = []
-        pass
+
+    def __init__(self):
+        self.chats = []
+
+    def get_chat(self, request_iterator, context):
+        lastindex = 0
+        while True:
+            while len(self.chats) > lastindex:
+                n = self.chats[last_message]
+                last_message += 1
+                yield n
     
-    def send_chat(self, request, context):
-        print(request)
-        return(pb2.Empty())
+    def send_chat(self, request: pb2.Chat, context):
+        print("[{}] {}".format(request.username, request.message))
+        self.chats.append(request)
+        return pb2.Empty()
 
 if __name__ == '__main__':
     port = 11921
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))  # create a gRPC server
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))  # create a gRPC server
     pb2_grpc.add_ChatBotServicer_to_server(Server(), server)  # register the server to gRPC
     print('Starting server. Listening...')
     server.add_insecure_port('localhost:11912')
     server.start()
-    server.wait_for_termination()
+    while True:
+        time.sleep(64 * 64 * 100)

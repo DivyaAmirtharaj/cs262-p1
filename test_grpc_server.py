794bd5cef08108de8bc96fdd47111ba77f58ea47
Divya <divya.amirtharaj@gmail.com>
2023-02-22 13:04:45 -0500

    uniqueness test

--- test_grpc_server.py
+++ test_grpc_server.py
@@ -4,6 +4,7 @@ from server import Server
 import protos.service_pb2_grpc as pb2_grpc
 import protos.service_pb2 as pb2
 import grpc
+from database import Database
 
 """
 Unit tests for the gRPC server
@@ -17,21 +18,35 @@ class ChatServerTest(unittest.TestCase):
         address = "localhost"
         port = 11912
         self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-        pb2_grpc.add_ChatBotServicer_to_server(Server(), server)
+        pb2_grpc.add_ChatBotServicer_to_server(Server(), self.server)
         self.server.add_insecure_port("{}:{}".format(address, port))
         self.server.start()
+    
+    def cleanTable(self):
+        self.database = Database()
+        self.database.delete_table()
+        self.database.create_table()
 
     def tearDown(self):
         self.server.stop(None)
 
     def test_server(self):
+        self.cleanTable()
         with grpc.insecure_channel(f'localhost:{self.port}') as channel:
             stub = pb2_grpc.ChatBotStub(channel)
             response = stub.server_create_account(pb2.User(username='divya', password='test'))
-        self.assertEqual(response.username, 'divya')
+            self.assertEqual(response.username, 'divya')
+    
+            response = stub.server_create_account(pb2.User(username='kat', password='test'))
+            self.assertEqual(response.username, 'kat')
+
+            response = stub.server_create_account(pb2.User(username='divya', password='test'))
+            self.assertEqual(response.username, '')
 
 
 if __name__ == "__main__":
     t = ChatServerTest()
+    t.setUp()
     t.test_server()
+    t.tearDown()
 
